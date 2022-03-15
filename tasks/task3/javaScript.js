92e368f9a7ddeedb2966ebd21971d9dcf91d060d
Anna Willer <79911632+AniaZab@users.noreply.github.com>
2022-03-15 14:19:16 +0100

    funkcja do wpisywania kółką i krzyżyka

--- tasks/task3/javaScript.js
+++ tasks/task3/javaScript.js
@@ -1,5 +1,12 @@
 let numberOfClicks = 0;
-let picture = "k";
-if(numberOfClicks % 2 == 0){
-  picture = "o";
+function Wstaw(buttonId) {
+  if (document.getElementById(buttonId).innerHTML.value == "0") {
+	  if(numberOfClicks % 2 == 0){
+		  document.getElementById(buttonId).innerHTML.value = "X";
+	  }
+	  else{
+		  document.getElementById(buttonId).innerHTML.value = "O";
+	  }
+	  numberOfClicks+=1;
+	}
 }
