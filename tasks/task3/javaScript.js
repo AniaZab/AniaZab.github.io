35410d3c7be89750098ce2930db026862b3c64be
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-03-18 21:53:26 +0100

    poprawka

--- tasks/task3/javaScript.js
+++ tasks/task3/javaScript.js
@@ -24,7 +24,7 @@ const clickCard = function() {
     document.getElementById(activeCard.id).value = cardsValues[whoseTurn];
     cards[activeCard.id] = cardsValues[whoseTurn];
     activeCard.classList.add(cardsSigns[whoseTurn]);
-    cards[parseInt(activeCard.id)].removeEventListener("click", clickCard);
+    this.removeEventListener("click", clickCard);
     //cards = cards.filter(card => card.classList.contains("hidden"));
     numberOfMoves = numberOfMoves + 1;
     console.log("kliknieta karta!");
