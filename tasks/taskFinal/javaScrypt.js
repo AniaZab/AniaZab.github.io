a4475bd4800f8f34a1d470c06a1945154f3ed156
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-05-24 22:22:40 +0200

    zmiana w javaScrypt

--- tasks/taskFinal/javaScrypt.js
+++ tasks/taskFinal/javaScrypt.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+function selectFromTable() {
     $('#dtBasicExample').DataTable();
     $('.dataTables_length').addClass('bs-select');
-});
+}
+$(document).ready(selectFromTable);
