94773c6244b653278070bb52858f7f7bae0b3e9f
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-05-31 13:38:01 +0200

    zmiana bez init

--- tasks/taskFinal/javaScrypt.js
+++ tasks/taskFinal/javaScrypt.js
@@ -1,9 +1,8 @@
 const pagesInWebsite = ["PageRegularEvents", "PageNewEvents", "PageRegister", "PageLogin", "PageAddEvent"];
 
-const init = function show(shown) {
+function show(shown) {
     pagesInWebsite.forEach(page => {
         document.getElementById(page).style.display = 'none'
     })
     document.getElementById(shown).style.display = 'block';
 }
-init("PageRegularEvents");
